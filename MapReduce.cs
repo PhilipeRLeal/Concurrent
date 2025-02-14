e1392b7d569baf79c4b1f56aaba77ec80c61e805
Philipe_Riskalla_Leal <leal.philipe@gmail.com>
2025-02-14 19:55:30 -0300

    Addition of description

--- MapReduce.cs
+++ MapReduce.cs
@@ -2,9 +2,27 @@
 
 namespace Concurrent
 {
+    /// <summary>
+    /// Class responsible for implementing the MapReduce policy.
+    /// 
+    /// Its implementation is solely based on the "System.Collections.Concurrent.Parallel.For" method. 
+    /// </summary>
     public static class MapReduce
     {
-        public static List<TResult> Run<T, TResult>(IEnumerable<T> seq, Func<T, TResult> map, Func<T, bool> reducerCondition)
+        /// <summary>
+        /// Executes the MapReduce operation.
+        /// </summary>
+        /// <typeparam name="T">The input type of <paramref name="seq"/></typeparam>
+        /// <typeparam name="TResult">The returned type after applying map over T</typeparam>
+        /// <param name="seq">The sequence of elements to be processed</param>
+        /// <param name="map">The mapping function that will be applied over <paramref name="seq"/> </param>
+        /// <param name="filter">The conditional that will be used to accumulate the mapped data. 
+        /// If default, no filtering will be applied; 
+        /// therefore, all elements of <paramref name="seq"/> will be processed. 
+        /// Otherwise, only those elements from <paramref name="seq"/> that return true 
+        /// for the <paramref name="filter"/> will be processed and later returned by this method</param>
+        /// <returns>The list of elements that were filtered by the <paramref name="filter"/> method and later transformed by the <paramref name="map"/> function.</returns>
+        public static List<TResult> Run<T, TResult>(IEnumerable<T> seq, Func<T, TResult> map, Func<T, bool>? filter = null)
         {
             var results = new ConcurrentBag<TResult>();
 
@@ -14,13 +32,22 @@ namespace Concurrent
             {
                 var elemento = seq.ElementAt(i);
 
-                var condition = reducerCondition(elemento);
+                if (filter is not null)
+                {
+                    var condition = filter(elemento);
 
-                if (condition)
+                    if (condition)
+                    {
+                        var transformed = map(elemento);
+                        lista.Add(transformed);
+                    }
+                }
+                else
                 {
                     var transformed = map(elemento);
                     lista.Add(transformed);
                 }
+                
 
                 return lista;
             };
